ff31834c0c31e3915f1bf62e3f09b2614992cb6e
Juan Valencia Calvellido <juanvalencia@calvellido.es>
2023-02-13 11:13:58 +0100

    Add the initial site scaffolding

---
+++ src/pages/about/what-is-arrow/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Placeholder } from '@site/src/components/Placeholder/Placeholder';
+
+
+export default function WhatIsArrow(): JSX.Element {
+  return (
+    <Placeholder title={`What is Arrow`} />
+  );
+}
