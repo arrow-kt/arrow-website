8ec326096562fd6d494dfd95f4a79f37188f8352
Simon Vergauwen <nomisRev@users.noreply.github.com>
2023-02-14 14:56:59 +0100

    Add initial typed errors document

---
+++ guide/src/test/kotlin/examples/example-typed-errors-01.kt
@@ -0,0 +1,35 @@
+// This file was automatically generated from typed-errors.md by Knit tool. Do not edit.
+package arrow.website.examples.exampleTypedErrors01
+
+import arrow.core.Either
+import arrow.core.Either.Left
+import arrow.core.Either.Right
+import arrow.core.right
+import arrow.core.raise.Raise
+import arrow.core.raise.either
+import arrow.core.raise.fold
+import io.kotest.assertions.fail
+import io.kotest.matchers.shouldBe
+
+object MyError
+
+val one: Either<MyError, Int> = 1.right()
+
+fun Raise<MyError>.one(): Int = 1
+
+val res = either { one() }
+
+fun Raise<MyError>.res(): Int = one.bind()
+
+fun main() {
+  when (res) {
+    is Left -> fail("No logical failure occurred!")
+    is Right -> res.value shouldBe 1
+  }
+
+  fold(
+    { res() },
+    { _: MyError -> fail("No logical failure occurred!") },
+    { i: Int -> i shouldBe 1 }
+  )
+}
---
+++ guide/src/test/kotlin/examples/test/TypedErrorsTest.kt
@@ -0,0 +1,55 @@
+// This file was automatically generated from typed-errors.md by Knit tool. Do not edit.
+package arrow.core.examples.test
+
+import io.kotest.core.spec.style.StringSpec
+import arrow.website.captureOutput
+import kotlinx.knit.test.verifyOutputLines
+
+class TypedErrorsTest : StringSpec({
+  "ExampleTypedErrors01" {
+    arrow.website.examples.exampleTypedErrors01.main()
+  }
+
+  "ExampleTypedErrors02" {
+    arrow.website.examples.exampleTypedErrors02.main()
+  }
+
+  "ExampleTypedErrors03" {
+    arrow.website.examples.exampleTypedErrors03.main()
+  }
+
+  "ExampleTypedErrors04" {
+    arrow.website.examples.exampleTypedErrors04.main()
+  }
+
+  "ExampleTypedErrors05" {
+    arrow.website.examples.exampleTypedErrors05.main()
+  }
+
+  "ExampleTypedErrors06" {
+    arrow.website.examples.exampleTypedErrors06.main()
+  }
+
+  "ExampleTypedErrors07" {
+    arrow.website.examples.exampleTypedErrors07.main()
+  }
+
+  "ExampleTypedErrors08" {
+    arrow.website.examples.exampleTypedErrors08.main()
+  }
+
+  "ExampleTypedErrors09" {
+    arrow.website.examples.exampleTypedErrors09.main()
+  }
+
+  "ExampleTypedErrors10" {
+    arrow.website.examples.exampleTypedErrors10.main()
+  }
+
+  "ExampleTypedErrors11" {
+    arrow.website.examples.exampleTypedErrors11.main()
+  }
+
+}) {
+  override fun timeout(): Long = 1000
+}
