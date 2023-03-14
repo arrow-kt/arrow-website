3ac3a7a181d19a64a461f48c5a87c33151333a3a
Juan Valencia Calvellido <juanvalencia@calvellido.es>
2023-03-14 12:24:04 +0100

    Remove commented code on DecoratedText

--- src/components/DecoratedText/DecoratedText.tsx
+++ src/components/DecoratedText/DecoratedText.tsx
@@ -14,22 +14,14 @@ export const DecoratedText = ({
   children,
   icon,
   title,
-}: DecoratedTextProps): JSX.Element => {
-  // const currentSidebarcategory = useCurrentSidebarCategory();
-
-  // console.log('DecoratedText currentSidebarcategory', currentSidebarcategory);
-  // console.log('DecoratedText title', title);
-  // console.log('DecoratedText icon', icon);
-
-  return (
-    <>
-      <img
-        className={styles.icon}
-        src={useBaseUrl(`/img/${icon}`)}
-        alt={'Icon'}
-        title={'Icon'}
-      />
-      {title ? title : children}
-    </>
-  );
-};
+}: DecoratedTextProps): JSX.Element => (
+  <>
+    <img
+      className={styles.icon}
+      src={useBaseUrl(`/img/${icon}`)}
+      alt={'Icon'}
+      title={'Icon'}
+    />
+    {title ? title : children}
+  </>
+);
