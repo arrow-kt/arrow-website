d8e536c530362315778257a747f1f897e540ecd7
Juan Valencia Calvellido <juanvalencia@calvellido.es>
2023-03-20 10:54:16 +0100

    Add Libraries page

--- src/pages/libraries/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { Placeholder } from '@site/src/components/Placeholder/Placeholder';
-
-export default function Libraries(): JSX.Element {
-  return <Placeholder title={`Libraries`} />;
-}
