71f623e7959cd19c8a9955d24181e67b19b14a00
Juan Valencia Calvellido <juanvalencia@calvellido.es>
2023-03-20 10:54:29 +0100

    Add support page

--- src/pages/community/support/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { Placeholder } from '@site/src/components/Placeholder/Placeholder';
-
-export default function Support(): JSX.Element {
-  return <Placeholder title={`Support`} />;
-}
