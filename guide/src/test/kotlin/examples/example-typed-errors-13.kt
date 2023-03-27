9ca9d38ef0c83c306717fd667dad813e3beb8767
Alejandro Serrano <trupill@gmail.com>
2023-03-27 11:40:14 +0200

    Information about (par)ZipOrAccumulate (#109)
    Including a reference to the parallel versions as requested in
    https://github.com/arrow-kt/arrow/pull/2957
    
    ---------
    
    Co-authored-by: Simon Vergauwen <nomisRev@users.noreply.github.com>

---
+++ guide/src/test/kotlin/examples/example-typed-errors-13.kt
@@ -0,0 +1,27 @@
+// This file was automatically generated from working-with-typed-errors.md by Knit tool. Do not edit.
+package arrow.website.examples.exampleTypedErrors13
+
+import arrow.core.Either
+import arrow.core.Either.Left
+import arrow.core.raise.either
+import arrow.core.raise.ensure
+import io.kotest.matchers.shouldBe
+
+sealed interface UserProblem {
+  object EmptyName: UserProblem
+  data class NegativeAge(val age: Int): UserProblem
+}
+
+data class User private constructor(val name: String, val age: Int) {
+  companion object {
+    operator fun invoke(name: String, age: Int): Either<UserProblem, User> = either {
+      ensure(name.isNotEmpty()) { UserProblem.EmptyName }
+      ensure(age >= 0) { UserProblem.NegativeAge(age) }
+      User(name, age)
+    }
+  }
+}
+
+fun example() {
+  User("", -1) shouldBe Left(UserProblem.EmptyName)
+}
