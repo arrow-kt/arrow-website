71cc9f07cd2120cac230feace8778c20dfac1c8e
Alejandro Serrano <trupill@gmail.com>
2024-10-05 22:36:41 +0200

    Accumulation

--- guide/src/test/kotlin/examples/example-typed-errors-20.kt
+++ guide/src/test/kotlin/examples/example-typed-errors-20.kt
@@ -25,4 +25,8 @@ data class User private constructor(val name: String, val age: Int) {
     }
   }
 }
+
+fun sample() {
+  User("", -1) shouldBe Left(nonEmptyListOf(UserProblem.EmptyName, UserProblem.NegativeAge(-1)))
+}
 fun example() { }
