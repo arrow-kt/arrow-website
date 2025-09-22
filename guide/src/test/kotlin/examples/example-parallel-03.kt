25185ebfa025f60e9e87e01cf8c110d4dedc031c
Alejandro Serrano <trupill@gmail.com>
2025-09-22 16:12:45 +0200

    Info about concurrency DSLs

--- guide/src/test/kotlin/examples/example-parallel-03.kt
+++ guide/src/test/kotlin/examples/example-parallel-03.kt
@@ -1,15 +1,12 @@
 // This file was automatically generated from parallel.md by Knit tool. Do not edit.
 package arrow.website.examples.exampleParallel03
 
-import arrow.fx.coroutines.parZip
-import arrow.fx.coroutines.await.awaitAll
-typealias UserId = Int
-data class User(val name: String, val avatar: String)
-suspend fun getUserName(id: UserId): String = "$id-name"
-suspend fun getAvatar(id: UserId): String = "$id-avatar"
+import arrow.core.merge
+import arrow.fx.coroutines.raceN
+suspend fun downloadFrom(url: String): Unit = Unit
 
-suspend fun getUser(id: UserId): User = awaitAll {
-  val name = async { getUserName(id) }
-  val avatar = async { getAvatar(id) }
-  User(name.await(), avatar.await())
-}
+suspend fun file(server1: String, server2: String) =
+  raceN(
+    { downloadFrom(server1) },
+    { downloadFrom(server2) }
+  ).merge()
--- guide/src/test/kotlin/examples/example-parallel-05.kt
+++ guide/src/test/kotlin/examples/example-parallel-05.kt
@@ -1,10 +1,12 @@
 // This file was automatically generated from parallel.md by Knit tool. Do not edit.
 package arrow.website.examples.exampleParallel05
 
+import kotlinx.coroutines.delay
+import arrow.core.raise.Raise
 import arrow.core.raise.either
 import arrow.fx.coroutines.parZip
-import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.delay
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.coroutines.cancellation.CancellationException
 
 suspend fun logCancellation(): Unit = try {
   println("Sleeping for 500 milliseconds ...")
@@ -15,10 +17,12 @@ suspend fun logCancellation(): Unit = try {
 }
 
 suspend fun example() {
-  val triple = parZip(
-    { either<String, Unit> { logCancellation() } },
-    { either<String, Unit> { delay(100); raise("Error") } },
-    { either<String, Unit> { logCancellation() } }
-  ) { a, b, c -> Triple(a, b, c) }
-  println(triple)
+  val res = either {
+    parZip(
+      { logCancellation() } ,
+      { delay(100); raise("Error") },
+      { logCancellation() }
+    ) { a, b, c -> Triple(a, b, c) }
+  }
+  println(res)
 }
